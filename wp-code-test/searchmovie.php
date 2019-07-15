80ea189de730025bd6178f43e4a464c114bf75be
Steven Star <stevenpstar@gmail.com>
2019-07-15 15:32:02 +1000

    Update searchmovie.php

--- wp-code-test/searchmovie.php
+++ wp-code-test/searchmovie.php
@@ -7,5 +7,5 @@
     $response = file_get_contents('https://api.themoviedb.org/3/search/movie?api_key='.$apikey.'&language=en-US&query='.$name.'&page=1&include_adult=false');
 
 
-    print_r($response); //$array->results;
+    print_r($response); 
 ?>
